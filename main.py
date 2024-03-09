3f78602bd85f319948007f8166ff0032aa54370e
Troley <troleyhs@gmail.com>
2024-03-09 01:14:10 +0100

    Add multi cell text to each PDF file from TxT Files - exercise done

--- main.py
+++ main.py
@@ -15,5 +15,10 @@ for filepath in filepaths:
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
     pdf.set_font(family="Times", style="B", size=24)
-    pdf.cell(w=0, h=12, txt=name, align="L")
+    pdf.cell(w=0, h=12, txt=name, align="L", ln=1)
+
+    # Add multi cell text from files
+    pdf.set_font(family="Arial", size=12)
+    pdf.multi_cell(w=0, h=5, txt=text)
+
     pdf.output(f"PDFs/{filename}.pdf")
